edcaede9538ca2fefd702ec9a0faeb2d378c86c6
Ade Yahya <adeyahyaprasetyo@gmail.com>
2018-12-17 20:39:24 +0700

    update

--- js/gallery.js
+++ js/gallery.js
@@ -1,39 +1,28 @@
 $(function() {
-  $(".img-w").each(function() {
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+  var items = [];
+  $(".img-w").each(function(index) {
     $(this).wrap("<div class='img-c'></div>")
-    let imgSrc = $(this).find("img").attr("src");
-     $(this).css('background-image', 'url(' + imgSrc + ')');
-  })
-            
-  
+    $(this).attr('index', index);
+    var imgSrc = $(this).find("img").attr("src");
+    var w = $(this).find("img").get(0).naturalWidth;
+    var h = $(this).find("img").get(0).naturalHeight;
+    items.push({src: imgSrc, w: w, h: h});
+    $(this).css('background-image', 'url(' + imgSrc + ')');
+  });
+
+  function openPhoto(index) {
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      index: index || 0 // start at first slide
+    };
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  }
+
   $(".img-c").click(function() {
-    let w = $(this).outerWidth()
-    let h = $(this).outerHeight()
-    let x = $(this).offset().left
-    let y = $(this).offset().top
-    
-    
-    $(".active").not($(this)).remove()
-    let copy = $(this).clone();
-    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
-    $(".active").css('top', y - 8);
-    $(".active").css('left', x - 8);
-    
-      setTimeout(function() {
-    copy.addClass("positioned")
-  }, 0)
-    
-  }) 
-  
-  
-
-  
-})
-
-$(document).on("click", ".img-c.active", function() {
-  let copy = $(this)
-  copy.removeClass("positioned active").addClass("postactive")
-  setTimeout(function() {
-    copy.remove();
-  }, 500)
+    var index = $(this).find(".img-w").attr("index");
+    openPhoto(index);
+  })
 })
